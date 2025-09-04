dc186f868cb5ba3f196ab60c7cac0de59f591943
Prachi Rathi <prachirathi3101@gmail.com>
2025-09-04 14:39:32 +0530

    Describe what you changed

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
-const express = require("express");
-const app = express();
-const PORT = 5001;
-
+const express=require("express");
+const app=express();
+const PORT=5000;
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello, Express is working");
+app.get("/",(req,res)=>{
+    res.send("Hello, Express is Working");
 });
-
-app.get("/about", (req, res) => {
-  res.json({ message: "this is express.js backend" });
+app.get("/abot",(req,res)=>{
+    res.json({message:"Hello this is express.js backend"});
 });
-
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+app.listen(PORT,()=>{
+    console.log('Server running on http://localhost:${PORT}');
+})
